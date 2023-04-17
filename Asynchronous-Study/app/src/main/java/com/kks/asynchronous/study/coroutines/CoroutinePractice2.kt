95c74c04661a9e0b72d211e7f2eefe4aefdaf475
김규성 <ksjmgrkks@gmail.com>
2023-04-17 20:56:46 +0900

    서스펜딩 함수 값을 리턴하는 병렬처리를 도와주는 async-await 코루틴의 계층구조로 인해, 자식 예외 발생 시 같은 계층의 형제 코루틴부터 부모 코루틴 까지 순차적으로 cancel되며 예외를 최상단 부모의 코루틴에 던짐

--- Asynchronous-Study/app/src/main/java/com/kks/asynchronous/study/coroutines/CoroutinePractice2.kt
+++ Asynchronous-Study/app/src/main/java/com/kks/asynchronous/study/coroutines/CoroutinePractice2.kt
@@ -1,7 +1,47 @@
 package com.kks.asynchronous.study.coroutines
 
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import kotlin.random.Random
 
 fun main() = runBlocking {
+//        val value1 = async(start = CoroutineStart.LAZY) { getRandom1() } // 바로 실행하지 않고, start 후에 실행함.
+//        val value2 = async(start = CoroutineStart.LAZY) { getRandom2() }
+//
+//        value1.start() // 큐에 수행 예약을 함.
+//        value2.start()
 
+        // 그냥 getRandom1(), getRandom2()를 각각 받아오면, 동시에 실행되지 않고,
+        // 2초 이상 걸리지만, async-await을 사용하게 되면, 동시에 작업하기 때문에 약 1초정도 걸린다.
+    try {
+        doSomething()
+    } catch (e: IllegalStateException) {
+        println("doSomething failed: $e") //4. 최종적으로 올라온 예외를 처리해줌
+    }
+}
+
+suspend fun getRandom1(): Int {
+    try {
+        delay(1000L)
+        return Random.nextInt(0, 500)
+    } finally {
+        println("getRandom1 is cancelled.")
+    }
+}
+
+suspend fun getRandom2(): Int {
+    delay(500L)
+    println("getRandom2 문제 발생")
+    throw IllegalStateException()
+}
+
+suspend fun doSomething() = coroutineScope {//3. 부모 코루틴 역시 캔슬
+    val value1 = async { getRandom1() } // 2. 형제 코루틴(getRandom2)이 문제 발생 -> 취소
+    val value2 = async { getRandom2() } // 1. 자식 코루틴 문제 발생
+
+    // deferred.await은 job.join + 결과 리턴 이라고 보면 됨
+    try {
+        println("${value1.await()} + ${value2.await()} = ${value1.await() + value2.await()}")
+    } finally {
+        println("doSomething is cancelled.")
+    }
 }
