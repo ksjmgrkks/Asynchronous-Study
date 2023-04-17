d3f0683b65dbd3f9b69a9822c051445dbc135535
김규성 <ksjmgrkks@gmail.com>
2023-04-17 17:56:16 +0900

    취소와 타임아웃1 job 취소는 cancel, 코루틴 수행 혹은 취소를 기다려주는 코드는 join -> 그래서 cancelAndJoin이라는 유틸도 있음 취소 가능한 코루틴을 만들어주고싶다면, isActive를 활용해 코루틴이 취소 되었는지 체크 가능함.

--- Asynchronous-Study/app/src/main/java/com/kks/asynchronous/study/coroutines/CoroutineStudy.kt
+++ Asynchronous-Study/app/src/main/java/com/kks/asynchronous/study/coroutines/CoroutineStudy.kt
@@ -1,34 +1,54 @@
 package com.kks.asynchronous.study.coroutines
 
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 fun main() = runBlocking {
-    doOneTwoThree()
-    println("runBlocking: ${Thread.currentThread().name}")
-    println("5!")
+    doCount()
 }
 
 suspend fun doOneTwoThree() = coroutineScope {
-    val job = launch {
+    val job1 = launch {
         println("launch1: ${Thread.currentThread().name}")
         delay(1000L) //suspension point
         println("3!")
     }
-    job.join() //suspension point
 
-    launch {
+    val job2 = launch {
         println("launch2: ${Thread.currentThread().name}")
         println("1!")
     }
-    repeat(1000) {
-        launch {
-            println("launch3: ${Thread.currentThread().name}")
-            delay(500L)
-            println("2!")
+
+    val job3 = launch {
+        println("launch3: ${Thread.currentThread().name}")
+        delay(500L)
+        println("2!")
+    }
+
+    delay(800L)
+    job1.cancel()
+    job2.cancel()
+    job3.cancel()
+    println("4!")
+}
+
+suspend fun doCount() = coroutineScope {
+    val job1 = launch(Dispatchers.Default) {
+        var i = 1
+        var nextTime = System.currentTimeMillis() + 100L
+        while (i <= 10 && isActive){
+            //isActive를 통해 코루틴이 동작할 때만 코드가 동작하게 할 수 있다.
+            val currentTime = System.currentTimeMillis()
+            if (currentTime >= nextTime) {
+                println(i)
+                nextTime = currentTime + 100L
+                i++
+            }
         }
     }
-    println("4!")
+
+    delay(300L)
+//    job1.cancel() // 코루틴 취소
+//    job1.join() // 코루틴이 실행되고, 취소되는것을 기다려줌
+    job1.cancelAndJoin() //cancel과 join을 한꺼번에 하는 코드
+    println("doCount Done!")
 }
