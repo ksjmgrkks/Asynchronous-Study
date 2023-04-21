a0e9d9f3ccab7f23463207a158bf66694e620ec6
김규성 <ksjmgrkks@naver.com>
2023-04-21 14:04:07 +0900

    Flow Context 추가 주석

--- Asynchronous-Study/app/src/main/java/com/kks/asynchronous/study/coroutines/flow/FlowPractice5.kt
+++ Asynchronous-Study/app/src/main/java/com/kks/asynchronous/study/coroutines/flow/FlowPractice5.kt
@@ -24,6 +24,14 @@ fun simple(): Flow<Int> = flow {
           }
       } */
 }.flowOn(Dispatchers.Default) // flowOn 연산자를 통해 컨텍스트를 올바르게 바꿀 수 있습니다. ↑ Upstream에 영향을 줌
+/* 출처: https://developer.android.com/kotlin/flow?hl=ko
+*  기본적으로 flow 빌더의 생산자는 수집(collect)하는 코루틴의 CoroutineContext에서 실행됩니다.
+*  앞에서 언급한 것처럼 다른 CoroutineContext에서 값을 emit할 수 없습니다.
+*  이 동작은 경우에 따라 원하지 않는 동작일 수도 있습니다.
+*  예를 들어, 이 주제 전체에 사용된 예에서 저장소 레이어는
+*  viewModelScope가 사용하는 Dispatchers.Main에서 작업을 실행하면 안 됩니다.
+* */
+
 /*
 *  flowOn은 업스트림 흐름의 CoroutineContext를 변경합니다.
 *  즉, 생산자 및 중간 연산자가 flowOn 전에(또는 위에) 적용됩니다.
