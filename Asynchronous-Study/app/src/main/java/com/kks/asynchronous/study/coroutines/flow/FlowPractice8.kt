eeac9ce581fa35f26af7e18a3d4f99d7ccb5f94d
김규성 <ksjmgrkks@naver.com>
2023-04-23 11:19:20 +0900

    플로우 플래트닝하기 추가 설명 주석

--- Asynchronous-Study/app/src/main/java/com/kks/asynchronous/study/coroutines/flow/FlowPractice8.kt
+++ Asynchronous-Study/app/src/main/java/com/kks/asynchronous/study/coroutines/flow/FlowPractice8.kt
@@ -18,6 +18,7 @@ fun requestFlow(i: Int): Flow<String> = flow {
 fun main() = runBlocking<Unit> {
 
     // flatMapConcat은 첫번째 요소에 대해서 플레트닝을 하고 나서 두번째 요소를 합니다.
+    // 하나 처리가 다 완료되고 나서 그 다음 처리를 해야할 때
     val startTime1 = System.currentTimeMillis() // remember the start time
     (1..3).asFlow().onEach { delay(100) } // a number every 100 ms
         .flatMapConcat {
@@ -28,6 +29,8 @@ fun main() = runBlocking<Unit> {
         }
 
     // flatMapMerge는 첫 요소의 플레트닝을 시작하며 이어 다음 요소의 플레트닝을 시작합니다.
+    // 굳이 하나 처리가 다 완료되고 나서 그 다음 처리를 할 필요가 없고,
+    // 최종 결과를 모두 받는 것이 중요하며, 속도가 중요할 때
     val startTime2 = System.currentTimeMillis() // remember the start time
     (1..3).asFlow().onEach { delay(100) } // a number every 100 ms
         .flatMapMerge {
@@ -38,6 +41,7 @@ fun main() = runBlocking<Unit> {
         }
 
     // flatMapLatest는 다음 요소의 플레트닝을 시작하며 이전에 진행 중이던 플레트닝을 취소합니다.
+    // 거래량, 주가처럼 최종 실시간 정보가 중요할 때
     val startTime3 = System.currentTimeMillis() // remember the start time
     (1..3).asFlow().onEach { delay(100) } // a number every 100 ms
         .flatMapLatest {
